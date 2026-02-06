736d5d3401e43a4a6e5005b715a54c874c62ac4b
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-06 01:00:42 +0000

    feat(raft): add __init__

---
+++ src/raft/__init__.py
@@ -0,0 +1,11 @@
+"""Raft consensus implementation (asyncio, gRPC, filesystem storage)."""
+
+__all__ = [
+    "config",
+    "core",
+    "rpc",
+    "storage",
+    "node",
+    "sm",
+    "util",
+]
