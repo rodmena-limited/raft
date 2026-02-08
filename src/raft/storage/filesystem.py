92aedaded738cf6861f8199f0ae4952bdaba2a89
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-08 00:12:18 +0000

    Add _read_log to filesystem

--- src/raft/storage/filesystem.py
+++ src/raft/storage/filesystem.py
@@ -16,3 +16,7 @@ class FsLogStorage(LogStorage):
             self.store_metadata(LogMetadata(term=0, voted_for=None, commit_index=0, last_applied=0))
         if not self.log_path.exists():
             self._write_log([])
+
+    def _read_log(self) -> list[LogEntryRecord]:
+        with self.log_path.open("rb") as f:
+            return pickle.load(f)
